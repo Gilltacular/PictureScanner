97638001649ffc7dbdd89a42b762ca583aecd668
Gilltacular <182698728+Gilltacular@users.noreply.github.com>
2026-07-30 11:38:05 -0500

    Fix duplicate HTML structure, use sex_session_start() for session hardening.

--- includes/header.php
+++ includes/header.php
@@ -2,56 +2,31 @@
     include_once 'includes/register.inc.php';
     include_once 'includes/functions.php';
 
-    session_start();
+    sec_session_start();
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Picture Scanner Home</title>
+        <title>Picture Scanner</title>
         <link rel="stylesheet" href="styles/main.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Latest compiled and minified CSS -->
+        <!-- Bootstrap 3 CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
         <!-- jQuery library -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-        <!-- Latest compiled JavaScript -->
+        <!-- Bootstrap 3 JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-        <!-- sha512 source -->
-        <script type="text/JavaScript" src="js/sha512.js"></script>
+        <!-- SHA512 hashing (client-side password protection) -->
+        <script type="text/javascript" src="js/sha512.js"></script>
 
-        <!--  -->
-        <script type="text/JavaScript" src="js/forms.js"></script>
-    </head>
-
-    <body>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Picture Scanner Home</title>
-        <link rel="stylesheet" href="styles/main.css" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-        <!-- jQuery library -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-        <!-- Latest compiled JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-        <!-- sha512 source -->
-        <script type="text/JavaScript" src="js/sha512.js"></script>
-
-        <!--  -->
-        <script type="text/JavaScript" src="js/forms.js"></script>
+        <!-- Form validation and hashing -->
+        <script type="text/javascript" src="js/forms.js"></script>
     </head>
 
     <body>
@@ -72,22 +47,17 @@
                 </div>
                 <div class="collapse navbar-collapse" id="myNavbar">
                     <ul class="nav navbar-nav">
-                        <li class="inactive">
-                            <a href="about.php">About</a>
-                        </li>
+                        <li><a href="about.php">About</a></li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- right side of nav bar items -->
-                        <div class="span7 text-center">
-                            <li>
-                                <?php if(login_check($mysqli) == false) : ?>
-                                    <button type="button" class="btn btn-default" onclick="parent.location='login.php'">Login</button>
-                                    <button type="button" class="btn btn-success" onclick="parent.location='register.php'">Register</button>
-                                <?php else : ?>
-                                    <button type="button" class="btn btn-default" onclick="parent.location='includes/logout.php'">Logout</button>
-                                <?php endif; ?>
-                            </li>
-                        </div>
+                        <li>
+                            <?php if(login_check($mysqli) == false) : ?>
+                                <button type="button" class="btn btn-default" onclick="parent.location='login.php'">Login</button>
+                                <button type="button" class="btn btn-success" onclick="parent.location='register.php'">Register</button>
+                            <?php else : ?>
+                                <button type="button" class="btn btn-default" onclick="parent.location='includes/logout.php'">Logout</button>
+                            <?php endif; ?>
+                        </li>
                     </ul>
                 </div>
             </div>
